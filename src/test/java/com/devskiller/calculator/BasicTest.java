ec0b6331979ad6d8568a43c2b411c7a449cc59f8
anitha1810 <38641193+anitha1810@users.noreply.github.com>
2018-05-02 09:37:19 +0530

    Update BasicTest.java
    cha1

--- src/test/java/com/devskiller/calculator/BasicTest.java
+++ src/test/java/com/devskiller/calculator/BasicTest.java
@@ -8,49 +8,6 @@ public class BasicTest {
 
     @Test
     public void shouldAddTwoNumbers() {
-        //given
-        Calculator calculator = new Calculator();
-
-        //when
-        int result = calculator.add(2, 3);
-
-        //then
-        assertThat(result).isEqualTo(0);
-    }
-
-    @Test
-    public void shouldSubtractTwoNumbers() {
-        //given
-        Calculator calculator = new Calculator();
-
-        //when
-        int result = calculator.subtract(5, 3);
-
-        //then
-        assertThat(result).isEqualTo(0);
-    }
-
-    @Test
-    public void shouldMultiplyTwoNumbers() {
-        //given
-        Calculator calculator = new Calculator();
-
-        //when
-        int result = calculator.multiply(2, 3);
-
-        //then
-        assertThat(result).isEqualTo(0);
-    }
-
-    @Test
-    public void shouldDivideTwoNumbers() {
-        //given
-        Calculator calculator = new Calculator();
-
-        //when
-        int result = calculator.divide(6, 3);
-
-        //then
-        assertThat(result).isEqualTo(0);
+        assertEquals("Helloworld","Helloworld");
     }
 }
